ba76e4d339864a80dc74b45cac44b4e2cf08fdc4
Johan Winther <johan.winther@outlook.com>
2018-10-04 17:18:12 +0200

    redirect to calendar

--- archive-event.php
+++ archive-event.php
@@ -6,8 +6,16 @@
  * @since ftek 2.0
  */
 
-/* Redirect to news page with event filter on */
-$url = '/nyheter?post_types[]=event';
+/* Redirect to calendar */
+/*
+$dates = array(
+    'yearnum' => get_query_var('year'),
+    'monthnum' => get_query_var('monthnum'),
+    'daynum' => get_query_var('day'),
+);
+$url = '/kalender/?'.http_build_query($dates, '&');
+*/
+$url = '/kalender/';
 wp_redirect( $url, 301); // Status code: 301 Permanently moved 
 exit;
 ?>
