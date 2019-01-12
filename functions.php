ce39315c58a2101d00a1fc1949e576268ea727b5
Johan Winther <johan.winther@outlook.com>
2019-01-12 17:29:51 +0100

    Hide profile picture in profile settings

--- functions.php
+++ functions.php
@@ -305,6 +305,7 @@ function ftek_disable_profile_fields() {
 				tr.user-comment-shortcuts-wrap, \
 				tr.user-email-wrap, \
 				tr.user-admin-color-wrap, \
+				tr.user-profile-picture \
 				h3:nth-of-type(1), h3:nth-of-type(1) + ul, \
 				h3:nth-of-type(2), h3:nth-of-type(2) + table, \
 				div#screen-meta-links, \
