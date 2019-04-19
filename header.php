364c32076001fcaa6ab3ae8df49b8ee65775ef9b
Eric Carlsson <32720134+ECarlsson@users.noreply.github.com>
2019-04-20 01:14:58 +0200

    Removed title from theme
    Removed title from theme as it is now set by yoast SEO

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?php wp_title( '|', true, 'right' ); ?> <?= bloginfo('name')?></title>
     <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" />
   	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
