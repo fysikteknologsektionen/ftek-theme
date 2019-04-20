c8febcbb9f1480d7446a317b6759c0b4748a3083
Eric Carlsson <32720134+ECarlsson@users.noreply.github.com>
2019-04-20 14:23:05 +0200

    Made title yoast SEO compatible

--- header.php
+++ header.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" />
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+    	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" />
   	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
