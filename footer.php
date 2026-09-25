ba66bd28c5d6ed22a53ddf537f854d2229d4144d
gabrielreimer <agabrielreimer@gmail.com>
2026-09-25 12:25:44 +0200

    jane street logo style match chs logo

--- footer.php
+++ footer.php
@@ -28,7 +28,7 @@
 			<a href="https://chalmersstudentkar.se"><img id="chs-logo" alt="Chalmers Studentkår" src="<?php bloginfo('stylesheet_directory'); ?>/images/chalmers.svg" /></a>
 		</div>
 		<div id="footer-chs-logo">
-			<a href="https://www.janestreet.com/"><img id="chs-logo" alt="Jane Street" src="<?php bloginfo('stylesheet_directory'); ?>/images/Jane_Street_Logo.png" /></a>
+			<a href="https://www.janestreet.com/"><img id="jane-logo" alt="Jane Street" src="<?php bloginfo('stylesheet_directory'); ?>/images/Jane_Street_Logo.png" /></a>
 		</div>
 		<div id="footer-development">
 			<p><?=__('Development', 'ftek')?>: Pontus Granström</p>
