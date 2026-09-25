ee064de12c6ebeeaf3cdc07e7e4cacf04c1f0e88
gabrielreimer <agabrielreimer@gmail.com>
2026-09-25 12:18:46 +0200

    added jane street logo

--- footer.php
+++ footer.php
@@ -27,6 +27,9 @@
 		<div id="footer-chs-logo">
 			<a href="https://chalmersstudentkar.se"><img id="chs-logo" alt="Chalmers Studentkår" src="<?php bloginfo('stylesheet_directory'); ?>/images/chalmers.svg" /></a>
 		</div>
+		<div id="footer-chs-logo">
+			<a href="https://www.janestreet.com/"><img id="chs-logo" alt="Jane Street" src="<?php bloginfo('stylesheet_directory'); ?>/images/Jane_Street_Logo.png" /></a>
+		</div>
 		<div id="footer-development">
 			<p><?=__('Development', 'ftek')?>: Pontus Granström</p>
 			<p><?=__('Development and design', 'ftek')?>: <a href="https://github.com/JohanWinther" target="_self">Johan Winther</a></p>
