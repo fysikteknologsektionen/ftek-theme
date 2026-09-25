31b8eb233f1d202465ef47a99aaee96b93e1c3e7
gabrielreimer <agabrielreimer@gmail.com>
2026-09-25 12:40:19 +0200

    test

--- footer.php
+++ footer.php
@@ -26,9 +26,9 @@
 		</div>
 		<div id="footer-chs-logo">
 			<a href="https://chalmersstudentkar.se"><img id="chs-logo" alt="Chalmers Studentkår" src="<?php bloginfo('stylesheet_directory'); ?>/images/chalmers.svg" /></a>
-		</div>
-		<div id="footer-chs-logo">
 			<a href="https://www.janestreet.com/"><img id="jane-logo" alt="Jane Street" src="<?php bloginfo('stylesheet_directory'); ?>/images/Jane_Street_Logo.png" /></a>
+		</div>
+		<div id="footer-jane-logo">
 		</div>
 		<div id="footer-development">
 			<p><?=__('Development', 'ftek')?>: Pontus Granström</p>
